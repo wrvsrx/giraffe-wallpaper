5b85e384a40278f4a49602a5ba517718cb10983d
wrvsrx <wrvsrx@outlook.com>
2023-01-07 18:41:50 +0800

    feat: update the script

--- Main.hs
+++ Main.hs
@@ -1,30 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Data.Text as T
-import qualified Data.Text.Read as T
 import System.Environment (getArgs)
-import Turtle
+import System.Process (callProcess)
 
-black = "black.png"
-squarePng = "giraffe.png"
-outputPng = "output.png"
-
+main :: IO ()
 main = do
   args <- getArgs
-  let svg = T.pack (head args)
-      width = read (args !! 1) :: Integer
-      height = read (args !! 2) :: Integer
-      pick = either error id
-      isWidthLarger = width > height
-      widthCenter = width * 2 `div` 3
-      heightCenter = height * 2 `div` 3
-      widthText = T.pack (show width)
-      heightText = T.pack (show height)
-      widthCenterText = T.pack (show widthCenter)
-      heightCenterText = T.pack (show heightCenter)
-  print svg
-  stdout
-    ( inproc "convert" ["-size", widthText <> "x" <> heightText, "xc:black", black] empty
-        <|> inproc "inkscape" (["--export-type", "png", "--export-filename", squarePng] <> (if isWidthLarger then ["-h", heightCenterText] else ["-w", widthCenterText]) <> [svg]) empty
-        <|> inproc "convert" [black, squarePng, "-gravity", "Center", "-composite", outputPng] empty
-    )
+  let
+    black = "black.png"
+    squarePng = "giraffe.png"
+    outputPng = "output.png"
+    svg = head args
+    width = read (args !! 1) :: Integer
+    height = read (args !! 2) :: Integer
+    scale x = x * 2 `div` 3
+  callProcess "convert" ["-size", show width <> "x" <> show height, "xc:black", black]
+  callProcess "inkscape" (["--export-type", "png", "--export-filename", squarePng] <> (if width > height then ["-h", show (scale height)] else ["-w", show (scale width)]) <> [svg])
+  callProcess "convert" [black, squarePng, "-gravity", "Center", "-composite", outputPng]
