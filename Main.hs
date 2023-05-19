0bd05dc95fcad8ac6d46269f7d4901cfed94dd47
wrvsrx <wrvsrx@outlook.com>
2023-05-19 18:10:18 +0800

    feat: export bmp instead of png

--- Main.hs
+++ Main.hs
@@ -7,13 +7,13 @@ main :: IO ()
 main = do
   args <- getArgs
   let
-    black = "black.png"
+    black = "black.bmp"
     squarePng = "giraffe.png"
-    outputPng = "output.png"
+    output = "output.bmp"
     svg = head args
     width = read (args !! 1) :: Integer
     height = read (args !! 2) :: Integer
     scale x = x * 2 `div` 3
   callProcess "convert" ["-size", show width <> "x" <> show height, "xc:black", black]
   callProcess "inkscape" (["--export-type", "png", "--export-filename", squarePng] <> (if width > height then ["-h", show (scale height)] else ["-w", show (scale width)]) <> [svg])
-  callProcess "convert" [black, squarePng, "-gravity", "Center", "-composite", outputPng]
+  callProcess "convert" [black, squarePng, "-gravity", "Center", "-composite", output]
