65a3b96f959d9ee95ee3e8333bbc4b9bbc510a40
wrvsrx <wrvsrx@outlook.com>
2025-04-06 21:10:01 +0800

    chore(build): remove dependencies to ghc

--- default.nix
+++ default.nix
@@ -1,24 +1,32 @@
-{ haskellPackages
-, inkscape
-, imagemagick
-, stdenv
-, width ? 3840
-, height ? 2160
+{
+  inkscape,
+  imagemagick,
+  stdenvNoCC,
+  width ? 3840,
+  height ? 2160,
 }:
-stdenv.mkDerivation {
+let
+  scale = x: x * 2 / 3;
+in
+stdenvNoCC.mkDerivation {
   name = "giraffe-wallpaper";
   buildInputs = [
-    haskellPackages.ghc
     inkscape
     imagemagick
   ];
   src = ./.;
   buildPhase = ''
-    runghc Main main.svg ${builtins.toString width} ${builtins.toString height}
+    convert -size ${builtins.toString width}x${builtins.toString height} xc:black black.png
+    inkscape --export-type png --export-filename giraffe.png ${
+      if width > height then
+        "-h ${builtins.toString (scale height)}"
+      else
+        "-w ${builtins.toString (scale width)}"
+    } main.svg
+    convert black.png giraffe.png -gravity center -composite output.png
   '';
   installPhase = ''
     mkdir -p $out/share
     cp output.png $out/share/wallpaper.png
   '';
 }
-
--- flake.nix
+++ flake.nix
@@ -2,16 +2,22 @@
   description = "flake template";
 
   inputs = {
-    nur-wrvsrx.url = "github:wrvsrx/nur-packages";
-    nixpkgs.follows = "nur-wrvsrx/nixpkgs";
-    flake-parts.follows = "nur-wrvsrx/flake-parts";
-  };
-
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" ];
-    perSystem = { pkgs, ... }: rec {
-      packages.default = pkgs.callPackage ./default.nix { };
-      devShells.default = pkgs.mkShell { inputsFrom = [ packages.default ]; };
+    nixpkgs.url = "github:wrvsrx/nixpkgs/patched-nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        { pkgs, ... }:
+        rec {
+          packages.default = pkgs.callPackage ./default.nix { };
+          devShells.default = pkgs.mkShell { inputsFrom = [ packages.default ]; };
+        };
+    };
 }
